e73140a9b04fa244c75625ed4da6df31fc0e2e7f
Karan3872 <karananuragi7879@gmail.com>
2025-01-14 13:51:01 +0530

    adding program 04 of remove duplicates from string in string handling

--- JavaStringHandling/RemoveDuplicates.java
+++ JavaStringHandling/RemoveDuplicates.java
@@ -6,37 +6,38 @@ public class RemoveDuplicates {
         // Create a Scanner object to take user input
         Scanner sc = new Scanner(System.in);
 
-        //the user to enter a string
+        // Prompt the user to enter a string
         System.out.println("Enter the string:");
         String s1 = sc.next(); // Read the user input string and store it in variable s1
 
-        String result="";
-		
-		
-		
-		for(int i=0;i<s1.length();i++)
-		{
-		    boolean isDuplicate=false;
-			char ch = s1.charAt(i);
-		
-			for(int j=0;j<result.length();j++)
-			{
-		
-				if(ch==result.charAt(j))
-				{
-					isDuplicate=true;
-					break;
-				}
-			}
-		
-			if(!isDuplicate)
-			{
-				result+=ch;
-			}
-		  
-		}
+        // Initialize an empty string to store the result without duplicates
+        String result = "";
 
-        System.out.println("String after remove duplicates : " + result);
+        // Loop through each character of the input string s1
+        for (int i = 0; i < s1.length(); i++) {
+            // Initialize a boolean flag to check if the character is a duplicate
+            boolean isDuplicate = false;
+            // Get the character at index i of the input string
+            char ch = s1.charAt(i);
+
+            // Inner loop to check if the character is already in the result string
+            for (int j = 0; j < result.length(); j++) {
+                // Compare the character ch with each character in the result string
+                if (ch == result.charAt(j)) {
+                    // If a match is found, set the flag to true and break the loop
+                    isDuplicate = true;
+                    break;
+                }
+            }
+
+            // If the character is not a duplicate, append it to the result string
+            if (!isDuplicate) {
+                result += ch;
+            }
+        }
+
+        // Display the result string after removing duplicates
+        System.out.println("String after removing duplicates: " + result);
 
         // Close the Scanner object to free up resources
         sc.close();
@@ -44,4 +45,5 @@ public class RemoveDuplicates {
 }
 
 
-//  input =karan; resut=kar 
+
+//  input =karan; resut=karn
